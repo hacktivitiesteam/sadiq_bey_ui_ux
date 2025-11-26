d2b01d5f14144c5a4c27a2bf694654c81fc4f315
Hacktivities <hacktivities.team@gmail.com>
2025-11-26 09:59:40 +0000

    birazda balaca et

--- src/components/app/app-header.tsx
+++ src/components/app/app-header.tsx
@@ -128,7 +128,7 @@ const AppHeader = ({ isAdmin = false, lang, setLang }: AppHeaderProps) => {
                 {lang && <ContactUs lang={lang} />}
                 <a href="/communication-aid" onClick={(e) => handleLinkClick(e, "/communication-aid")}>
                     <Button asChild variant="ghost" size="icon" aria-label="Communication Aid">
-                         <PenSquare className="h-6 w-6" />
+                         <PenSquare className="h-5 w-5" />
                     </Button>
                 </a>
                 <ReadingModeToggle />
